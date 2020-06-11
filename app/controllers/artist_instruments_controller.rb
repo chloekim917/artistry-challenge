b7388b1bfbad607250724fe17d94e9fc945dc7f0
chloekim917 <chloekim917@gmail.com>
2020-06-11 19:27:44 -0400

    Done.

--- app/controllers/artist_instruments_controller.rb
+++ app/controllers/artist_instruments_controller.rb
@@ -1,25 +1,22 @@
 class ArtistInstrumentsController < ApplicationController
-    # def index
-    #     @ais = ArtistInstrument.all
-    # end
-    
     def new
-        @ai = ArtistInstrument.new
-
+        @artist_instrument = ArtistInstrument.new
+    
         @artists = Artist.all
         @instruments = Instrument.all
     end
-
+    
     def create
-        ai = ArtistInstrument.create(ai_params)
-        redirect_to artist_path(ai.artist_id)
+        artist_instrument = ArtistInstrument.create(ai_params)
+      
+        redirect_to artist_path(artist_instrument.artist)
     end
-
+    
     private
-
+    
     def ai_params
         params.require(:artist_instrument).permit(:artist_id, :instrument_id)
     end
-
+    
  
 end
